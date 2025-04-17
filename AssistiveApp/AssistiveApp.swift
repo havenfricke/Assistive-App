568a0c5b954902ff5a504c528480357324366cc4
Sean Perkins <smichaelperkins@gmail.com>
2025-04-17 14:51:36 -0600

    Simple AppRouter Toggle
    Main Screen Toggle for App VIew. Needs to be connected to Kevin's Staff View Post Merge.

--- AssistiveApp/AssistiveApp.swift
+++ AssistiveApp/AssistiveApp.swift
@@ -24,7 +24,7 @@ struct AssistiveApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppRouterView()
         }
         .modelContainer(sharedContainer)
     }
